6b0896ef644f98c2c8e3a2a5f5c916bf5d93d87a
y-ichikawa <y_ichikawa@shtech.co.jp>
2025-06-05 15:26:34 +0900

    first

---
+++ src/main/java/com/example/taskSample/TaskSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskSample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskSample/TaskSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
