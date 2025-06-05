4a4316b164e1a932a8328e8c87abc2c14d49eb92
y-ichikawa <y_ichikawa@shtech.co.jp>
2025-06-05 15:36:17 +0900

    controllerの修正

--- src/main/java/com/example/taskSample/controller/HomeController.java
+++ src/main/java/com/example/taskSample/controller/HomeController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
